be5265064e156ee0f527153c27e7d04a31a8d3ef
davodyadegari <davoud.yadegari88@gmail.com>
2023-11-30 12:23:34 +0330

    amoozesh

---
+++ src/test/java/com/davod/amoozeshghah/AmoozeshghahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davod.amoozeshghah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmoozeshghahApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
